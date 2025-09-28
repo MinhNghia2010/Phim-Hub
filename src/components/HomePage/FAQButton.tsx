7630733b1827b53cb2b8684905be021a8c1828e5
Your Name <minhnghia2010200500@gmail.com>
2025-09-28 13:46:56 +0700

    Update FAQButton.tsx

--- src/components/HomePage/FAQButton.tsx
+++ src/components/HomePage/FAQButton.tsx
@@ -9,7 +9,7 @@ function FAQButton() {
   return (
     <Button
       className={cn(
-        "bg-red-45 m-0 ml-20 h-min w-min px-3 py-3 text-sm font-bold text-white lg:px-[14px] lg:py-5 xl:px-[18px] xl:py-6 xl:text-lg",
+        "bg-red-45 m-0 h-min w-min px-3 py-3 text-sm font-bold text-white lg:px-[14px] lg:py-5 xl:px-[18px] xl:py-6 xl:text-lg",
         "hover:bg-red-800 active:bg-red-950",
       )}
       onClick={handleClick}
